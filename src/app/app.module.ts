9818eb3bd0594a0961cc4419330e05c1f1f15633
Vinicius Petris <vinicius.petris@b2wdigital.com>
2023-05-28 16:03:26 -0300

    adicionado dois novos componentes para a atividade 07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
